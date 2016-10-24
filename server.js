a230f99045de23c76da3a1ff1d6d3122752f0031
shenq-feng.kwan <shenq-feng.kwan@hpe.com>
2016-10-24 13:47:52 +0800

    nodejs express

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var http = require('http')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.get('/test/', function (req, res) {
+  res.end('Hello World -- 01 \n');
+})
+
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var server = app.listen(port, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
+Date.prototype.addDays = function(days)
+{
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
