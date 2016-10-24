06487d24cd5fce03e88ff3507c6c5eee81636f01
shenq-feng.kwan <shenq-feng.kwan@hpe.com>
2016-10-24 11:31:10 +0800

    nodejs hello world port 80

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http')
-var port = process.env.PORT || 1337;
+var port = 80;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
